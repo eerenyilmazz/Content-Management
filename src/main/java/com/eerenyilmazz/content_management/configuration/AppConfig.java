97ab2a46f467ac31334a06111e107e5f9d321542
Eren Yılmaz <eerenyilmazz1@gmail.com>
2024-07-25 13:44:09 +0300

    edit/model-mapper

--- src/main/java/com/eerenyilmazz/content_management/configuration/AppConfig.java
+++ src/main/java/com/eerenyilmazz/content_management/configuration/AppConfig.java
@@ -1,6 +1,8 @@
 package com.eerenyilmazz.content_management.configuration;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.Converter;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,12 +12,24 @@ public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.addConverter(new Converter<String, Long>() {
+            @Override
+            public Long convert(MappingContext<String, Long> context) {
+                try {
+                    return Long.parseLong(context.getSource());
+                } catch (NumberFormatException e) {
+                    return null;
+                }
+            }
+        });
+
+        return modelMapper;
     }
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
 }
